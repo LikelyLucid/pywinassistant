0b7cb4a5dce7401845ce2dea0cdf0fd0a9347439
Arthur Mckellar <micoolplays@gmail.com>
2024-05-09 02:36:59 +0000

    May 9, 2024, 2:36 PM

--- core/ollama_url.py
+++ core/ollama_url.py
@@ -1,9 +1,10 @@
-from core.ollama_calls import get_openai_url
+# from core.ollama_calls import get_openai_url
 
 VISION_MODEL = "llava:34b"
 MAIN_MODEL = "llama3:70b"
 
-url = get_openai_url(MAIN_MODEL, VISION_MODEL)
+# url = get_openai_url(MAIN_MODEL, VISION_MODEL)
+url = "http://74.82.31.213:11434/api/"
 
 def get_url():
     return url, MAIN_MODEL, VISION_MODEL
